960f54bea790a886d44e510cf30be575206f2a33
DLWILLIAMS99 <DANIEL.L.WILLIAMS.99@gmail.com>
2023-05-23 15:34:50 +0000

    Sync before changine environments

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# standard shebang
+
+# here is the list that is referenced
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# The list is numbered starting with zero
+# The 3 print commands combine two strings using a list item and quotes
+print("The first item in the list (IP): " + my_list[0] )
+print("The second item in the list (port): " + str(my_list[1] )
+print("The last item in the list (state): " + my_list[2] )
+
