5d72135d6d5a58ca4504976f1441bbba80ab5791
DLWILLIAMS99 <daniel.l.williams.99@gmail.com>
2023-05-23 20:30:12 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -3,10 +3,11 @@
 
 # here is the list that is referenced
 my_list = [ "192.168.0.5", 5060, "UP" ]
-
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 # The list is numbered starting with zero
 # The 3 print commands combine two strings using a list item and quotes
-print("The first item in the list (IP): " + my_list[0] )
-print("The second item in the list (port): " + str(my_list[1] )
-print("The last item in the list (state): " + my_list[2] )
+# print("The first item in the list (IP): " + my_list[0] )
+# print("The second item in the list (port): " + str(my_list[1]) )
+# print("The last item in the list (state): " + my_list[2] )
+print(my_list[0] + " :  " + iplist[3] + " : " + iplist[4] )
 
