d95f381f35cbbfcd0bbf542dbb2b2a820e1aefd0
DLWILLIAMS99 <daniel.l.williams.99@gmail.com>
2023-05-24 22:27:29 +0000

    New script that makes a python template

---
+++ pythontemplate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A simple script to create a new python script with best practices prefilled at top and bottom
+Dan Williams | GitHub.com/DLWILLIAMS99"""
+# standard library imports go here first (if any)
+# import shutil
+# import os
+def main():
+    """called at runtime"""
+#   first stuff goes here
+    main() # this calls the actual main function
