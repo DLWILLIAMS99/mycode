d116fbb459e52b39824fea400c795710b377a505
dlwilliams99 <daniel.l.williams.99@gmail.com>
2023-05-30 09:57:07 -0700

    Did my gui work?

--- megaGITscript.py
+++ megaGITscript.py
@@ -1,44 +1,54 @@
 #!/usr/bin/env python3
-git status
-git commit -m "$name"
-git push mycode HEAD
-
+import sys
 from tkinter import *
-from tkinter import ttk
-# launch tkinter to create a simple GROUPREF_UNI_IGNORE
-# git status
+from tkinter.ttk import *
+# imports tkinter to create a simple graphical user interface. The wildcard imports all the classes, functions, and variables inside the tkinter module while not having to speciify whether to use the older tk or the newer ttk. It will automagically use the newer if available.
+
+def create():
+ # create a new script template by running the pytemp.py script
+    print("Creating your new template...")
+ #    os.system("python3 pytemp.py")
+    print("Done!")
+
+def status():
+ # git status
+    print("git status")
+
+def commit():
 # git commit -m "$name"
+    input("Enter a commit message: ")
+    print("git commit -m $name")
+
+def push():
 # git push mycode HEAD
+    print("git push mycode HEAD")
+
+def escape():
+# exit the program
+    print("Exiting...")
+    sys.exit()  
 
 def main():
-    root = Tk()
-    root.title('megaGITscript')
-    root.geometry("600x500")
-    def create():
-        # create a GROUPREF_UNI_IGNORE
-        print("create a GROUPREF_UNI_IGNORE")
-    def status():
-        # git status
-        print("git status")
-    def commit():
-        # git commit -m "$name"
-        print("git commit -m $name")
-    def push():
-        # git push mycode HEAD
-        print("git push mycode HEAD")
+    window = Tk()
+    window.title('megaGITscript')
+    window.geometry("600x500")
+    # create an entry widget
+    entry = Entry(window, width=30)
+    entry.pack(pady=20)
     # create a label
-    label = ttk.Label(root, text="megaGITscript", font=("Helvetica", 20))
-    label.pack(pady=20)
+    greeting = Label(window, text="What would you like to do?", font=("Helvetica", 20))
+    greeting.pack(pady=20)
     # create a frame
-    frame = Frame(root)
+    frame = Frame(window)
     frame.pack()
     # create buttons
-    create_button = ttk.Button(frame, text="Create GROUPREF_UNI_IGNORE", command=create)
+    create_button = Button(frame, text="Create New Template", command=create)
     create_button.grid(row=0, column=0, padx=5, pady=5)
-    status_button = ttk.Button(frame, text="git status", command=status)
+    status_button = Button(frame, text="git status", command=status)
     status_button.grid(row=0, column=1, padx=5, pady=5)
-    commit_button = ttk.Button(frame, text="git commit -m $name", command=commit)
+    commit_button = Button(frame, text="git commit -m $name", command=commit)
     commit_button.grid(row=1, column=0, padx=5, pady=5)
-    push_button = ttk.Button(frame, text="git push mycode HEAD", command=push)
+    push_button = Button(frame, text="git push mycode HEAD", command=push)
     push_button.grid(row=1, column=1, padx=5, pady=5)
-    root.mainloop()
+    exit_button = Button(frame, text="Exit", command=escape)
+    window.mainloop()
